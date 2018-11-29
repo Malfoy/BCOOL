9e7cf88e2d0f0d80c5117cb05fe36a55d1aea7d9
Malfoy <antoine.limasset@gmail.com>
2018-11-29 14:49:17 +0100

    minor fix for the test

--- test/test.sh
+++ test/test.sh
@@ -4,14 +4,13 @@
 CORES=4
 
 
-gzip -d lambda_virus.200bp.50x.1percentError.fa.gz
 
 rm -rf testFixedK/ testNtcard/
 
 
 
 # Start Bcool withoyt ntcard
-../Bcool.py  -u lambda_virus.200bp.50x.1percentError.fa -k 61  -o testFixedK -t $CORES
+../Bcool  -u lambda_virus.200bp.50x.1percentError.fa.gz -k 61  -o testFixedK -t $CORES
 
 printf "\n\n\n"
 # Start Bcool with ntcard
@@ -20,11 +19,11 @@ if [ -f "testFixedK/reads_corrected.fa" ];
 then
   echo "IT WORKS without ntcard !";
 else
-   echo "FAIL the install wen wrong"
+   echo "FAIL the install went wrong"
 fi
 printf "\n\n\n"
 
-../Bcool.py  -u lambda_virus.200bp.50x.1percentError.fa  -o testNtcard -t $CORES
+../Bcool  -u lambda_virus.200bp.50x.1percentError.fa  -o testNtcard -t $CORES
 
 printf "\n\n\n"
 
